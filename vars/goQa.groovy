1afdb762579899815538b52913b1ab7d227eb8bd
alx <alxibra@gmail.com>
2020-07-10 10:43:19 +0700

    fixing goQa.groovy

--- vars/goQa.groovy
+++ vars/goQa.groovy
@@ -1,38 +1,45 @@
 def call() {
-  parallel {
-    stage('Test') {
-      agent {
-        docker {
-          image 'golang:1.14'
-          label 'slave'
-          args '--user root'
+  pipeline {
+    agent any
+    stages {
+      stage ('QA'){
+        parallel {
+          stage('Test') {
+            agent {
+              docker {
+                image 'golang:1.14'
+                label 'slave'
+                args '--user root'
+              }
+            }
+            when {
+              not {
+                branch 'master'
+              }
+            }
+            steps {
+                sh 'go test -v ./...'
+            }
+          }
+          stage('linter') {
+            agent {
+              docker {
+                image 'golang:1.14'
+                label 'slave'
+                args '--user root'
+              }
+            }
+            when {
+              not {
+                branch 'master'
+              }
+            }
+            steps {
+                sh 'go get -u golang.org/x/lint/golint'
+                sh 'golint -set_exit_status ./...'
+            }
+          }
         }
-      }
-      when {
-        not {
-          branch 'master'
-        }
-      }
-      steps {
-          sh 'go test -v ./...'
-      }
-    }
-    stage('linter') {
-      agent {
-        docker {
-          image 'golang:1.14'
-          label 'slave'
-          args '--user root'
-        }
-      }
-      when {
-        not {
-          branch 'master'
-        }
-      }
-      steps {
-          sh 'go get -u golang.org/x/lint/golint'
-          sh 'golint -set_exit_status ./...'
       }
     }
   }
