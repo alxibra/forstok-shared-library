cead8f260dfb0845176f02b6b47e7690c2ed820c
alx <alxibra@gmail.com>
2020-07-10 10:12:34 +0700

    add goQa script

---
+++ vars/goQa.groovy
@@ -0,0 +1,41 @@
+def call() {
+    stage ('QA'){
+      parallel {
+        stage('Test') {
+          agent {
+            docker {
+              image 'golang:1.14'
+              label 'slave'
+              args '--user root'
+            }
+          }
+          when {
+            not {
+              branch 'master'
+            }
+          }
+          steps {
+              sh 'go test -v ./...'
+          }
+        }
+        stage('linter') {
+          agent {
+            docker {
+              image 'golang:1.14'
+              label 'slave'
+              args '--user root'
+            }
+          }
+          when {
+            not {
+              branch 'master'
+            }
+          }
+          steps {
+              sh 'go get -u golang.org/x/lint/golint'
+              sh 'golint -set_exit_status ./...'
+          }
+        }
+      }
+    }
+}
