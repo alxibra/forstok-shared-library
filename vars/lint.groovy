f8f7aa6473f914f55187295e5442ac885b22f2b5
alxibra <alxibra@forstok.com>
2022-03-10 19:52:24 +0700

    fixing comparation

--- vars/lint.groovy
+++ vars/lint.groovy
@@ -1,7 +1,7 @@
 def call() {
   checkout scm
   node ('slave') {
-    if(env.BRANCH_NAME =! 'master') {
+    if(env.BRANCH_NAME != 'master') {
       docker.image('alxibra/forstok-webhook-order-channel:0.0.4').withRun('--user root') { c ->
         sh 'golangci-lint run'
         sh 'golint -set_exit_status ./...'
