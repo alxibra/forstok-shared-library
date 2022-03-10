fa8e0f639a69faafd7fef648a5152c586839c522
alxibra <alxibra@forstok.com>
2022-03-10 20:31:06 +0700

    use customer image image

--- vars/lint.groovy
+++ vars/lint.groovy
@@ -1,9 +1,10 @@
 def call() {
   node ('slave') {
     if(env.BRANCH_NAME != 'master') {
-      docker.image('golang:1.16.3').withRun('--user root') { c ->
-        docker.image('golang:1.16.3').inside("--user root") {
-            sh 'go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.40.1'
+      docker.image('alxibra/forstok-webhook-order-channel:0.0.4').withRun() { c ->
+        docker.image('alxibra/forstok-webhook-order-channel:0.0.4').inside("--user root") {
+            sh 'golangci-lint run'
+            sh 'golint -set_exit_status ./...'
         }
       }
     }
