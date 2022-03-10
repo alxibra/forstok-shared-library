bffaa9a9a47e86ba2ba09452a3ce2bbb49da965a
alxibra <alxibra@forstok.com>
2022-03-10 21:17:01 +0700

    remove stage from deployPolling.groovy

--- vars/deployPolling.groovy
+++ vars/deployPolling.groovy
@@ -1,9 +1,7 @@
 def call() {
   node ('master') {
-    stage('polling') {
-      if(env.BRANCH_NAME == 'master') {
-        sh 'forstok deploy --cluster polling'
-      }
+    if(env.BRANCH_NAME == 'master') {
+      sh 'forstok deploy --cluster polling'
     }
   }
 }
