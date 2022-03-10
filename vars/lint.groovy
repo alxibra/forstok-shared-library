76f18735058da9812601bc5ca283990ee655bbb1
alxibra <alxibra@forstok.com>
2022-03-10 20:03:08 +0700

    use inside

--- vars/lint.groovy
+++ vars/lint.groovy
@@ -1,11 +1,10 @@
 def call() {
-  checkout scm
   node ('slave') {
     if(env.BRANCH_NAME != 'master') {
       docker.image('golang:1.16.3').withRun('--user root') { c ->
-        sh 'go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.40.1'
-        sh 'golangci-lint run'
-        sh 'golint -set_exit_status ./...'
+        docker.image('golang:1.16.3').inside("--user root") {
+            sh 'go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.40.1'
+        }
       }
     }
   }
