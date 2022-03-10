6013721cfc08d7da964f4eb478599b9116bbd275
alxibra <alxibra@forstok.com>
2022-03-10 19:48:27 +0700

    add lint

---
+++ vars/lint.groovy
@@ -0,0 +1,12 @@
+def call() {
+  checkout scm
+  node ('slave') {
+    if(env.BRANCH_NAME =! 'master') {
+      docker.image('alxibra/forstok-webhook-order-channel:0.0.4').withRun('--user root') { c ->
+        sh 'golangci-lint run'
+        sh 'golint -set_exit_status ./...'
+      }
+    }
+  }
+}
+
