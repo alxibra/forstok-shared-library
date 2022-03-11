53d1def9b9d8c36e82bfd2b676887d2b435640f3
alxibra <alxibra@forstok.com>
2022-03-11 12:00:08 +0700

    checout in lint

--- vars/lint.groovy
+++ vars/lint.groovy
@@ -1,5 +1,6 @@
 def call() {
   node ('slave') {
+    checkout scm
     if(env.BRANCH_NAME != 'master') {
       docker.image('alxibra/forstok-webhook-order-channel:0.0.4').withRun() { c ->
         docker.image('alxibra/forstok-webhook-order-channel:0.0.4').inside("--user root") {
