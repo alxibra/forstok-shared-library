25c07f5df4e5a63ffb3b8df754a4d5b661dcf7c7
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-05-12 06:39:02 +0000

    update node choice in build function

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,6 @@
 def call(String arch) {
-  node('slave') {
+  def nodeId = ['arm': 'arm', 'x86': 'slave']
+  node(nodeId[arch]) {
     checkout scm
     if(env.BRANCH_NAME == 'master') {
       sh 'forstok build --arch ' + arch
